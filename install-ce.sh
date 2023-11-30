7f875647edaad27a3c5069e8c68271691707cf96
SelfHarmony <tdnttd@mail.ru>
2023-11-30 20:46:14 +0300

    Update install-ce.sh

--- install-ce.sh
+++ install-ce.sh
@@ -2,6 +2,8 @@
 
 # Step 1: Install Docker
 sudo apt update
+sudo apt upgrade
+sudo apt install-y net-tools
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
